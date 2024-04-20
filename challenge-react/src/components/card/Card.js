787ce401f3dab55acc2dbc968f31a6febf9dd3f7
Nutchapon Hanouypornlert <nutchaponhanouypornlert@Nutchapons-MacBook-Pro.local>
2024-04-20 16:15:33 +0700

    step4: create TamboonCard component

--- challenge-react/src/components/card/Card.js
+++ challenge-react/src/components/card/Card.js
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 const StyledDiv = styled.div`
   margin: 10px;
   border: 1px solid #ccc;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 2px 4px, rgba(0, 0, 0, 0.1) 0px 2px 4px;
+  position: relative;
 `;
 
 const Card = ({ children, ...props }) => {
--- challenge-react/src/components/card/TamboonCard.js
+++ challenge-react/src/components/card/TamboonCard.js
@@ -3,30 +3,6 @@ import styled from 'styled-components';
 
 import Card from './Card';
 
-const StyledImage = styled.img`
-  width: 100%;
-  height: 250px;
-`;
-
-const ActionBarLayout = styled.div`
-  display: flex;
-  padding: 10px;
-  justify-content: space-between;
-`;
-
-const TamboonCharityImage = ({ src, alt }) => {
-  return <StyledImage src={src} alt={alt} />;
-};
-
-const TamboonActionBar = ({ name, onClick }) => {
-  return (
-    <ActionBarLayout>
-      <span>{name}</span>
-      <button onClick={onClick}>Donate</button>
-    </ActionBarLayout>
-  );
-};
-
 const TamboonCard = ({
   payments,
   item,
@@ -37,21 +13,114 @@ const TamboonCard = ({
   const [onDonate, setOnDonate] = useState(false);
   const imgPath = `/images/${item.image}`;
 
-  const paymentChoice = payments.map((amount, j) => (
-    <label key={j}>
-      <input type="radio" name="payment" onClick={() => handleSelect(amount)} />
-      {amount}
-    </label>
-  ));
+  const toggle = () => {
+    setOnDonate(!onDonate);
+  };
+
+  const onDonateSubmit = () => {
+    toggle();
+    handlePay();
+  };
+
+  const onDonateChoiceSelect = (amount) => {
+    handleSelect(amount);
+  };
 
   return (
     <Card>
-      <div>
+      <TamboonContent isDonate={onDonate}>
         <TamboonCharityImage src={imgPath} alt={item.name} />
-        <TamboonActionBar name={item.name} onClick={() => setOnDonate(true)} />
-      </div>
+        <TamboonActionBar name={item.name} onClick={toggle} />
+      </TamboonContent>
+      <TamboonDonate
+        choice={payments}
+        onDonate={onDonate}
+        onSubmit={onDonateSubmit}
+        onSelect={onDonateChoiceSelect}
+      />
     </Card>
   );
 };
 
 export default TamboonCard;
+
+const TamboonContent = styled.div`
+  opacity: ${(props) => (props.isDonate ? 0.1 : 1)};
+`;
+
+const DonateLayout = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+
+  letter-spacing: 0.5px;
+
+  display: ${(props) => (props.isDonate ? 'flex' : 'none')};
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+`;
+
+const DonateChoiceSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 12px;
+`;
+
+const TamboonDonate = ({ onDonate, choice = [], onSubmit, onSelect }) => {
+  const paymentChoice = choice.map((amount, j) => (
+    <label key={j}>
+      <input type="radio" name="payment" onClick={() => onSelect(amount)} />
+      {amount}
+    </label>
+  ));
+
+  return (
+    <DonateLayout isDonate={onDonate}>
+      <span>Select the amount to donate (USD)</span>
+      <DonateChoiceSection>{paymentChoice}</DonateChoiceSection>
+      <button onClick={onSubmit}>Pay</button>
+    </DonateLayout>
+  );
+};
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: 300px;
+`;
+
+const TamboonCharityImage = ({ src, alt }) => {
+  return <StyledImage src={src} alt={alt} />;
+};
+
+const ActionBarLayout = styled.div`
+  display: flex;
+  padding: 24px;
+  justify-content: space-between;
+`;
+
+const ActionBarButton = styled.button`
+  background: none repeat scroll 0 0 transparent;
+  list-style: none outside none;
+  text-decoration: none;
+
+  color: #24a0ed;
+  border: 2px solid #24a0ed;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const TamboonActionBar = ({ name, onClick }) => {
+  return (
+    <ActionBarLayout>
+      <span>{name}</span>
+      <ActionBarButton onClick={onClick}>Donate</ActionBarButton>
+    </ActionBarLayout>
+  );
+};
